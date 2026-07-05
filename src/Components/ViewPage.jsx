0e8e1ef9e9bf74106057a8b0a663e2f606fdade1
Asmaa Elmekawy <asmaa1182004@gmail.com>
2026-07-05 22:28:04 +0300

    remove button

--- src/Components/ViewPage.jsx
+++ src/Components/ViewPage.jsx
@@ -1,12 +1,11 @@
-import { Button } from 'bootstrap';
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 
 function ViewPage() {
-    const {id}=useParams()
-    const [task,setTask]=useState(null)
+  const { id } = useParams()
+  const [task, setTask] = useState(null)
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
@@ -15,38 +14,38 @@ function ViewPage() {
       setTask(current);
     }
   }, [id]);
-  if(!task) return <p>not found</p>;
+  if (!task) return <p>not found</p>;
   return (
     <>
-    <div data-aos="fade-up" className='viewsec mt-5'>
-    <h1 className='text-center mb-5'>View your task</h1>
-    <div className='container col-lg-6 col-md-6 innerviewsec'>
-        <div className='content '>
+      <div data-aos="fade-up" className='viewsec mt-5'>
+        <h1 className='text-center mb-5'>View your task</h1>
+        <div className='container col-lg-6 col-md-6 innerviewsec'>
+          <div className='content '>
             <div>
-              <label style={{color:'#00adf5'}}>Title:</label><br/>
+              <label style={{ color: '#00adf5' }}>Title:</label><br />
               <p className='ms-1 values'>{task.name}</p>
             </div>
             <div>
-              <label style={{color:'#00adf5'}} >Description:</label><br/> 
+              <label style={{ color: '#00adf5' }} >Description:</label><br />
               <p className='ms-1 values'>{task.description || "No decription yet..."}</p>
             </div>
             <div>
-              <label style={{color:'#00adf5'}} >Status:</label><br/> 
+              <label style={{ color: '#00adf5' }} >Status:</label><br />
               <p className='ms-1 values'>{task.status}</p>
 
             </div>
             <div className='text-center'>
-            <button className='my-5 w-25 rounded btn' onClick={()=>navigate("/")}>OK</button>
+              <button className='my-5 w-25 rounded btn' onClick={() => navigate("/")}>OK</button>
 
 
             </div>
 
-        </div>
-        
-    </div>
+          </div>
 
-    </div>
-    
+        </div>
+
+      </div>
+
     </>
   )
 }
